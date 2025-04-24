1625d3370dbd5a0d7688826a876d23178895c7f0
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-04-24 12:01:50 +0530

    done with signin end point

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,16 +4,19 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import { connectToDB, UserModel } from "./db";
 import { JWT_SECRET } from "./config";
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
 app.use(express.json());
 
 connectToDB();
 
-app.post("/api/v1/signup", async (req, res) => {
+app.post("/api/v1/signup", async (req: Request, res: Response) => {
   const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    await UserModel.create({ username, password });
+    await UserModel.create({ username, password: hashedPassword });
 
     res.json({
       message: "User created successfully",
@@ -24,6 +27,31 @@ app.post("/api/v1/signup", async (req, res) => {
     });
   }
 });
+
+app.post("/api/v1/signin", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(401).json({ message: "Invalid password" });
+      return;
+    }
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ message: "Signin successful", token });
+  } catch (e) {
+    console.error("Signin error:", e);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
